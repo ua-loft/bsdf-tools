9a1313efd0b3d82e855817b4d5b4f068384765d2
Jacob P. Krell <jakepkrell@gmail.com>
2026-05-30 18:43:49 -0700

    adding blacklab code (what RSG runs prior to delivering a data file)

---
+++ processing/preprocessing/blacklab/compute_BL_angles.m
@@ -0,0 +1,74 @@
+function outputs = compute_BL_angles(inputs)
+%{
+This function computes stage angles for the black lab given source/view
+zenith/azimuth angles as input.  This function replaces Nathan Leisso's
+code 'nates_compute_BL_angles.m'.  This function does the opposite
+calculation as 'stage2angles.m'.
+
+    Input:
+        inputs (4 element vector of source/view zenith/azimuth angles in
+        degrees)
+    Output:
+        outputs (4 element row vector of stage configuration angles in
+        degrees)
+
+This function must be in the same folder as function file:
+    rodrigues.m
+
+Josie Maxwell, 12 May 2020, RSG
+%}
+
+% For debugging, paste zenith/azimuth angles here and comment out function
+% header
+% inputs = [20	150	45	252];
+
+% phi means zenith and theta is azimuth
+s_zen = inputs(1); s_azi = inputs(2); v_zen = inputs(3); v_azi = inputs(4);
+
+% axes of panel; azimuth measured from +x axis, zenith from +z axis
+X = [1, 0, 0];
+Y = [0, 1, 0];
+Z = [0, 0, 1];
+
+% Compute Source and View vectors
+S = [sind(s_zen)*cosd(s_azi), sind(s_zen)*sind(s_azi), cosd(s_zen)];
+V = [sind(v_zen)*cosd(v_azi), sind(v_zen)*sind(v_azi), cosd(v_zen)];
+
+% Compute Horizontal and Up vectors (describing west and up in black lab)
+U = normr(cross(S, V));
+if U(3) < 0
+    U = -U;
+end
+H = normr(cross(U, S));
+
+% Define rotation matrix to switch from panel to room coord system
+R = [S; H; U];
+
+% Transform panel vectors into room coordinate system
+x = R*X'; y = R*Y'; z = R*Z';
+s = R*S'; v = R*V';
+
+% Calculate radiometer angle
+rad_ang = acosd(dot(S, V)); % angle between source and view vectors
+if v(2) < 0 % if radiometer is to the east of the stage
+    rad_ang = -rad_ang;
+end
+
+% Compute tilt angle
+tilt_ang = abs(asind(z(3))); % tilt is always positive bc panel only tilts back
+
+% Compute yoke angle
+z2 = normr([z(1) z(2) 0]); % projection of panel normal onto horizontal plane
+yoke_ang = acosd(dot(z2,s)); 
+if z2(2) < 0 % if panel is facing east of the stage
+    yoke_ang = -yoke_ang;
+end
+
+% Compute panel rotation angle
+yy = rodrigues(Y',Z',yoke_ang); % find axis about which panel is tilted
+pan_rot_ang = acosd(dot(x,yy)); % angle between panel 'x' and its home position
+if x(3) > 0
+    pan_rot_ang = - pan_rot_ang; % panel rotates clockwise
+end
+
+outputs = [rad_ang yoke_ang tilt_ang pan_rot_ang];
