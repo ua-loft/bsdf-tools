7b2c5495e5c9135d9c39375c02653f3b73610432
Jacob P. Krell <jakepkrell@gmail.com>
2026-06-08 14:39:46 -0700

    added self-shading filter to blacklab angle code

--- processing/preprocessing/blacklab/angle_conversions/compute_BL_angles.m
+++ processing/preprocessing/blacklab/angle_conversions/compute_BL_angles.m
@@ -16,6 +16,11 @@ This function must be in the same folder as function file:
     rodrigues.m
 
 Josie Maxwell, 12 May 2020, RSG
+
+% =========================================================================
+
+% JPK == Jacob P. Krell, 2026/06/08-2026/xx/xx, LOFT;
+
 %}
 
 % For debugging, paste zenith/azimuth angles here and comment out function
@@ -34,12 +39,19 @@ Z = [0, 0, 1];
 S = [sind(s_zen)*cosd(s_azi), sind(s_zen)*sind(s_azi), cosd(s_zen)];
 V = [sind(v_zen)*cosd(v_azi), sind(v_zen)*sind(v_azi), cosd(v_zen)];
 
+% [JPK] Check if source and view are nearly collinear:
+if norm(cross(S,V)) < 1e-12
+    warning("S and V are collinear. Manually setting stage angles to 0, which is not true coordinate transformation but will be filtered out later regardless since radiometer shades source.");
+    outputs = [0 0 0 0];
+    return
+end
+
 % Compute Horizontal and Up vectors (describing west and up in black lab)
-U = normr(cross(S, V));
+U = normr_internal(cross(S, V));
 if U(3) < 0
     U = -U;
 end
-H = normr(cross(U, S));
+H = normr_internal(cross(U, S));
 
 % Define rotation matrix to switch from panel to room coord system
 R = [S; H; U];
@@ -58,7 +70,7 @@ end
 tilt_ang = abs(asind(z(3))); % tilt is always positive bc panel only tilts back
 
 % Compute yoke angle
-z2 = normr([z(1) z(2) 0]); % projection of panel normal onto horizontal plane
+z2 = normr_internal([z(1) z(2) 0]); % projection of panel normal onto horizontal plane
 yoke_ang = acosd(dot(z2,s)); 
 if z2(2) < 0 % if panel is facing east of the stage
     yoke_ang = -yoke_ang;
@@ -71,4 +83,21 @@ if x(3) > 0
     pan_rot_ang = - pan_rot_ang; % panel rotates clockwise
 end
 
+% [JPK] Snap values:
+tol = 1e-5; % threshold of what is considered 'numerical error'
+for snap_target = [-180, -90, 0, 90, 180]
+    pan_rot_ang(abs(pan_rot_ang - snap_target) < tol) = snap_target;
+end
+
 outputs = [rad_ang yoke_ang tilt_ang pan_rot_ang];
+
+function v_out = normr_internal(v)
+    % [JPK] Added to handle 'normr' failing to return zero vector.
+    if all(v == 0) % if zero vector
+        v_out = v; % return zero vector, not [0.57, 0.57, 0.57]
+    else
+        v_out = v / norm(v); % normalize
+    end
+end
+
+end
